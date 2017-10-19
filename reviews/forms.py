2577eef52136811d0df192fe3428bc54c687189c
Don Namgyal <don.namgyal@hotmail.com>
2017-10-19 17:37:58 -0400

    production ready

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,13 +2,12 @@ from django.forms import ModelForm, Textarea
 from reviews.models import Review, Landlord
 
 class ReviewForm(ModelForm):
-	class Meta:
-		model = Review
-		fields = ['address','rating',  'apt_condition', 'communication','maintenance_eff', 'rent_again', 'comment']
- 		widgets = {
-            'comment': Textarea(),
-        }
+    class Meta:
+        model = Review
+        fields = ['address', 'rating', 'apt_condition', 'communication', 'maintenance_eff', 'rent_again', 'comment']
+        widgets = {'comment': Textarea(),}
+
 class AddLandlord(ModelForm):
-	class Meta:
-		model = Landlord
-		fields = ['name']
+    class Meta:
+        model = Landlord
+        fields = ['name']
